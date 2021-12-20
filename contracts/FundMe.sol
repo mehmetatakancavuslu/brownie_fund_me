07d8b9a77524b169817d70f3794ae1b00d0a7b04
mehmetatakancavuslu <atakancavuslu322@gmail.com>
2021-12-21 01:30:02 +0300

    Add FundMe contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -25,7 +25,7 @@ contract FundMe {
         funders.push(msg.sender);
     }
 
-    function getVerison() public view returns (uint256) {
+    function getVersion() public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         );
